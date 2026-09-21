23551cf13394e2547e85f06c2ea177905bae0051
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2026-09-21 08:40:47 +0200

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const URL_API = 'https://petrik-idojaras-default-rtdb.europe-west1.firebasedatabase.app/.json'
+
+async function fetch_API(URL_API: string) {
+  const response = await fetch(URL_API);
+  if (!response.ok) {
+    throw new Error('Error: ' + response.statusText);
+  }
+  return await response.text();
+}
+
